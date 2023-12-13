b92957a1d66e365986e18d040e0ebd14cb3198c6
letuyen2102 <135399020+letuyen2102@users.noreply.github.com>
2023-12-13 14:45:28 +0700

    Update app.js

--- app.js
+++ app.js
@@ -21,6 +21,25 @@ app.use(cookieParser())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(mongoSanitize());
+app.use(function (req, res, next) {
+  // Lấy IP của người dùng
+  const ip = req.ip;
+
+  // Lấy URL của yêu cầu
+  const url = req.url;
+
+  // Lấy phương thức của yêu cầu
+  const method = req.method;
+
+  // Console.log thông tin về yêu cầu
+  console.log(`IP: ${ip}`);
+  console.log(`URL: ${url}`);
+  console.log(`Phương thức: ${method}`);
+
+  // Tiếp tục xử lý yêu cầu
+  next();
+});
+
 app.use('/myway/api/products', productRouter)
 app.use('/myway/api/users' , userRouter)
 
